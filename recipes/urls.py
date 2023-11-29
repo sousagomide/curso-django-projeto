6c8b67dcda4352765c3a85f44c0233c27def3e6e
Rodrigo Gomide <sousagomide@gmail.com>
2023-11-29 09:05:08 -0300

    2023-11-29-0905-update

--- recipes/urls.py
+++ recipes/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 
 from recipes.views import home
 
+from . import views
+
 urlpatterns = [
-    path('', home)
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe),
     
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -5,3 +5,8 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Gomide'
     })
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'name': 'Gomide'
+    })
