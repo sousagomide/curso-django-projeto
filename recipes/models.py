8890b27ba5b2664e245be50d9f507bdfab02d7b6
sousagomide <sousagomide@gmail.com>
2023-12-01 17:03:17 -0300

    2023-12-01-1703-update

--- recipes/models.py
+++ recipes/models.py
@@ -1,4 +1,27 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=65)
+
+
 class Recipe(models.Model):
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerField()
+    preparation_time_unit = models.CharField(max_length=65)
+    servings = models.IntegerField()
+    servings_unit = models.CharField(max_length=65)
+    preparation_step = models.TextField()
+    preparation_step_is_html = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True) 
+    update_at = models.DateTimeField(auto_now=True)
+    is_published = models.BooleanField(default=False)
+    cover = models.ImageField(upload_to='recipes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=False)
+
+
     
