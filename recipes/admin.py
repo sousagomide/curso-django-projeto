98d73a7b76e5ae6fe4cd58389461d8c6cf432f1f
Rodrigo Gomide <sousagomide@gmail.com>
2023-12-04 17:07:46 -0300

    2023-12-04-1707-update

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Category
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Category, CategoryAdmin)
